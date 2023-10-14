e311b8f754c3a1e9cdad9485db5611f9625a334f
Andre Nunes <andred_nunes@hotmail.com>
2023-10-14 10:08:58 -0300

    atualização na documentação

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,10 +1,24 @@
+/* <h1> Desafio Dio </h1>
+ * <p>Nesse desafio foi proposto a criação de um sistema que simulasse o cadastro de uma conta bancária, aplicando tudo o que foi aprendido até aqui.</sp>
+ * 
+ * @author Andre Dias
+ * @version 1.0
+ * @since 13/10/2023
+ */
+
+
 import java.util.Locale;
 import java.util.Scanner;
 
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
         
-        //declaração de variáveis 
+        /*<p> Descrição das variaveis</p>
+         * @param numero numero da conta bacária
+         * @param agencia codigo da agencia
+         * @param nomeCliente nome do cliente 
+         * @param saldo saldo em conta do cliente 
+         */
       
 
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);

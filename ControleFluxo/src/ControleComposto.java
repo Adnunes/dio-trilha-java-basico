123cb2ba1c3f96f3733f91fe69938b188d56275e
Andre Nunes <andred_nunes@hotmail.com>
2023-10-14 20:27:07 -0300

    estudo de condicionais

---
+++ ControleFluxo/src/ControleComposto.java
@@ -0,0 +1,16 @@
+
+
+public class ControleComposto {
+    public static void main(String[] args) throws Exception {
+        int nota=4;
+
+        if(nota >=7)
+            System.out.println("Aprovado");
+        
+        else if(nota >= 5 && nota<7)
+            System.out.println("Recuperação");
+
+        else
+            System.out.println("Reprovado");
+    }
+}
